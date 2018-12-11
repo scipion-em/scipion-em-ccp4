bb1b961a8b40a9d1d125623e9a68df72ee802aae
marta <mmmtnez@cnb.csic.es>
2018-12-11 17:59:01 +0100

    Addition of CCP4_BINARIES and CCP4_URL constants

--- ccp4/constants.py
+++ ccp4/constants.py
@@ -3,3 +3,10 @@ CCP4_HOME_DEFAULT='/usr/local/ccp4-7.0'
 
 #Supported version
 V7_0 = '7.0.056'
+
+# CCP4 used programs, we will check that they exist
+CCP4_BINARIES={
+    'COOT' : 'coot',
+    'REFMAC' : 'refmac5',
+    'PDBSET' : 'pdbset'}
+CCP4_URL='http://www.ccp4.ac.uk/download'
