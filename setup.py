db5bbfc5abe51d80597bad725b157266df4c8dfc
Marta Martinez <mmmtnez@users.noreply.github.com>
2019-04-12 12:29:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,12 +40,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.3b',  # Required
+    version='1.0.4b',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='''Programs from CCP4 used for model building in electronmicroscopy bbbbbbbbbbbbbbbbbbbb
+    description='''PPPPrograms from CCP4 used for model building in electronmicroscopy bbbbbbbbbbbbbbbbbbbb
     qweqwre rwetrwet retrtyrt rtyerty rteyrteye rteyrytr yrtyretyt yrtyeyrtyrt retyrteyerty retyretyre yrteyrtyrt
     ''',  # Required
 
