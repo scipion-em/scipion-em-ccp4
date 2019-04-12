eb9152ef5f77a4e0860f9e869fdc9909de6e542f
Marta Martinez <mmmtnez@users.noreply.github.com>
2019-04-12 12:24:57 +0200

    testing extra long comments

--- setup.py
+++ setup.py
@@ -40,12 +40,14 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.2',  # Required
+    version='1.0.3b',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Programs from CCP4 used for model building in electronmicroscopy',  # Required
+    description='''Programs from CCP4 used for model building in electronmicroscopy bbbbbbbbbbbbbbbbbbbb
+    qweqwre rwetrwet retrtyrt rtyerty rteyrteye rteyrytr yrtyretyt yrtyeyrtyrt retyrteyerty retyretyre yrteyrtyrt
+    ''',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
