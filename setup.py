000457b382b912165c4c1d3b53e5908576a57c55
Marta Martinez <mmmtnez@users.noreply.github.com>
2019-04-15 15:25:56 +0200

    Update setup.py with version 1.0.8

--- setup.py
+++ setup.py
@@ -40,12 +40,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.7',  # Required
+    version='1.0.8',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='CCP4 model building programs. Warning: Install CCP4 v. 7.0.56 or higher',  # Required
+    description='CCP4 model building programs in Scipion. Warning: Install CCP4 v. 7.0.56 or higher',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
